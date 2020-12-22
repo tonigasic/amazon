b5be7dfde6fad330109350c2938546bb6f825806
gasict <togasic@gmail.com>
2020-12-22 15:48:02 +0100

    Basket item frontend + functionality Login frontend checkout page finished

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from "react";
 import './Checkout.css';
 import Subtotal from './Subtotal';
+import CheckoutProduct from "./CheckoutProduct";
+import {useStateValue} from "./StateProvider";
 
 function Checkout() {
+    const [{basket}, dispatch] = useStateValue();
+
     return (
         <div className="checkout">
             <div className="checkout__left">
@@ -15,6 +19,15 @@ function Checkout() {
                     <h2 className="checkout__title">
                         Your shopping Basket
                     </h2>
+                    {basket.map(item => (
+                        <CheckoutProduct
+                            id={item.id}
+                            price={item.price}
+                            image={item.image}
+                            rating={item.rating}
+                            title={item.title}
+                        />
+                    ))}
                 </div>
             </div>
             <div className="checkout__right">
---
+++ src/Login.js
@@ -0,0 +1,50 @@
+import React, {useState} from "react";
+import './Login.css';
+import {Link} from "react-router-dom";
+
+function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = (e) => {
+        e.preventDefault();
+
+
+    };
+
+    const register = (e) => {
+        e.preventDefault();
+
+
+    };
+
+    return (
+        <div className="login">
+            <Link to='/'>
+                <img
+                    className="login__logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+                    alt=""
+                />
+            </Link>
+            <div className="login__container">
+                <h1>Sign-in</h1>
+                <form>
+                    <h5>E-Mail</h5>
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
+
+                    <h5>Password</h5>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+
+                    <button type="submit" className="login__signInButton" onClick={signIn}>Sign in</button>
+                </form>
+                <p>
+                    By signing-in you agree to the Amazon Fake Clone's Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
+                <button onClick={register} className="login__registerButton">Create you Amazon Account</button>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
