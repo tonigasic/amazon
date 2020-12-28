055a8f36425386d03c8b1ca7546a63278653813c
tonigasic <togasic@gmail.com>
2020-12-28 15:08:35 +0100

    Payment checkout page

---
+++ src/Payment.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import "./Payment.css";
+import {useStateValue} from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+import {Link} from "react-router-dom";
+
+function Payment() {
+    const [{basket, user}, dispatch] = useStateValue();
+
+    return (
+        <div className="payment">
+            <div className="payment__container">
+                <h1>Checkout (<Link to="/checkout">{basket?.length} items</Link>)</h1>
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Delivery Address</h3>
+                    </div>
+                    <div className="payment__address">
+                        <p>{user?.email}</p>
+                        <p>123 React Park</p>
+                        <p>München, DE</p>
+                    </div>
+                </div>
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Review items and delivery</h3>
+                    </div>
+                    <div className="payment__items">
+                        {basket.map(item => (
+                            <CheckoutProduct
+                                id={item.id}
+                                price={item.price}
+                                image={item.image}
+                                rating={item.rating}
+                                title={item.title}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Payment Method</h3>
+                    </div>
+                    <div className="payment__details">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Payment;
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,8 +3,10 @@ import './Subtotal.css';
 import CurrencyFormat from "react-currency-format";
 import {useStateValue} from "./StateProvider";
 import {getBasketTotal} from "./Reducer";
+import { useHistory } from "react-router-dom";
 
 function Subtotal() {
+    const history = useHistory();
     const [{basket}] = useStateValue();
 
     return (
@@ -26,7 +28,7 @@ function Subtotal() {
                 displayType={"text"}
                 prefix={"€"}
             />
-            <button>Proceed to Checkout</button>
+            <button onClick={e => history.push('/payment')}>Proceed to Checkout</button>
         </div>
     )
 }
